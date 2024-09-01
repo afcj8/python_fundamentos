f098fce7910a89c7a696eef4f0aad10679a92e2c
afcj8 <contatoantoniojunior8@gmail.com>
2024-09-01 17:58:24 -0300

    finalizado: listas

--- 09 - Listas/acessar_elementos.py
+++ 09 - Listas/acessar_elementos.py
@@ -1,5 +1,5 @@
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 primeiro_elemento = lista[0]    # Acessa o primeiro elemento (1)
 ultimo_elemento = lista[-1]     # Acessa o último elemento (10)
-print(primeiro_elemento)
-print(ultimo_elemento)
+print(primeiro_elemento)    # Imprime o primeiro elemento
+print(ultimo_elemento)  # Imprime o último elemento
